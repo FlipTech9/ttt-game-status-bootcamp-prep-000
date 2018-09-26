8ff532eaa651c8066daf752b24b1d4f7d9cdca2c
Alex S <alex.sunga@fliptech.co>
2018-09-26 08:04:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,31 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row 
+  [3,4,5], #Middle row 
+  [6,7,8], #Bottom row
+  [0,3,6], #Left column
+  [1,4,7], #Middle column
+  [2,5,8], #Right column
+  [6,4,2], #Diagonal one 
+  [0,4,8] #Diagonal two
+]
+
+def won?(board)
+  
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  postiion_3 = board[win_index_3]
+  
+  if
+    position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return WIN_COMBINATIONS[0]
+  else
+    false 
+  end
+end
